0015b8031b88e8eeb38d8f2877a87ea6c2008898
Lilia Busowa <liliabusowa@yandex.ru>
2022-10-08 20:02:16 +0700

    errors

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
-const defaultErrorText = 'Произошла ошибка';
-const userNotFoundErrorText = 'Пользователь с указанным _id не найден';
-const cardNotFoundErrorText = 'Карточка с указанным _id не найдена';
-const invalidDataErrorText = 'Переданы некорректные данные';
+const defaultErrorText = 'Произошла ошибка'; // 500
+const userNotFoundErrorText = 'Пользователь с указанным _id не найден'; // 404
+const cardNotFoundErrorText = 'Карточка с указанным _id не найдена'; // 404
+const invalidDataErrorText = 'Переданы некорректные данные'; // 400
 
 module.exports = {
   defaultErrorText, userNotFoundErrorText, cardNotFoundErrorText, invalidDataErrorText,
